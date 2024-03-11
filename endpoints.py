bcb9c0de4fbcc5c93433f9741bf0c6bb162cc2cb
Abhijith Mallya <abhijithmallya@gmail.com>
2024-03-11 23:38:22 +0530

    backend pending tasks

--- endpoints.py
+++ endpoints.py
@@ -1,23 +1,30 @@
 from fastapi import FastAPI, File, UploadFile
 from typing import Annotated
 
+# ----------------------------------------------------------
+#-----------------------Irshad ----------------------------
+# ----------------------------------------------------------
 
-
+#Load the model  before app = FastAPI()
 
 
 app = FastAPI()
 
 
+#Ignore this
 @app.get("/")
 def welcome():
     return {"message": "Server is Up !!"}
 
-
+#Ignore this 
 @app.post("/files/")
 async def create_file(file: Annotated[bytes, File()]):
     return {"file_size": len(file)}
 
 
+
+#Modify this part
+#Pass the uploaded file to the model
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     size = file.size/(1024)
@@ -26,3 +33,7 @@ async def create_upload_file(file: UploadFile):
             "File_content" : file.content_type, 
             "File size (Kb)": size 
             }
+    
+    #Actual return function as below
+    #result = answer given by the model
+    #return {"caption" : result}
